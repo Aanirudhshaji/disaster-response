e56c844ded4823916a3034dd6539b4cda623ef89
Anirudh shaji <144817702+Aanirudhshaji@users.noreply.github.com>
2025-06-20 00:46:53 +0530

    Update DisasterForm.jsx

--- disaster-frontend/src/components/DisasterForm.jsx
+++ disaster-frontend/src/components/DisasterForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:5001";
+const API = "https://disaster-response-wj1d.onrender.com";
 
 export default function DisasterForm({ onCreated }) {
   const [title, setTitle] = useState("");
